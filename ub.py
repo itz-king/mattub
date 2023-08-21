bc6f555456dfa24bcce16caed55a1f63eab0ccee
Armand Anderson <115343979+itz-king@users.noreply.github.com>
2023-08-21 15:42:36 +0000

    save

--- ub.py
+++ ub.py
@@ -32,10 +32,12 @@ me=ultroid.get_entity("me")
 logger.info(f"Connected Successfully As - {me.first_name} ({me.username}) !!!")
 logger.info("»«»«»«»«»»«»«»«»«»»«»«»«»«»")
 logger.info("Starting To Load Modules !!!")
-thumb=udB.get_key('THUMB')
-if not thumb:
+def thumbnail():
+    thumb=udB.get_key('THUMB')
+    if not thumb:
         thumb="https://telegra.ph/file/9bb02a1a7420241a61e88.jpg"
-os.system(f"curl -s -o 'thumb.jpg' '{thumb}'")
+    return thumb
+os.system(f"curl -s -o 'thumb.jpg' '{thumbnail()}'")
 def ultroid_cmd(pattern, owner_only=False,sudo_also=False):
     def decorator(func):
         async def wrapper(event):
